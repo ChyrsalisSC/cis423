d20193c52952fee4ee7947e10c7d9903e1e303fb
ChrysalisSC <80729716+ChyrsalisSC@users.noreply.github.com>
2022-01-19 00:15:08 -0800

    Updated with transformers

--- library.py
+++ library.py
@@ -2,3 +2,80 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'RenamingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_,
+                               prefix=self.target_column,   
+                               prefix_sep='_',    
+                               columns=[self.target_column],
+                               dummy_na = self.dummy_na,   
+                               drop_first=  self.drop_first  
+                               )
+    #assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    #print("works")
+    return X_
+
+  def fit(self, X, y = None): 
+    print("Warning: OHETransformer.fit does nothing.")
+    return X
+ 
+  def fit_transform(self, X, y= None): #was stuck for like 2 hours becuase i had (self,x) not (self,X,y=None)
+    result = self.transform(X)
+    return result
+  
+  
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'DropColumnsTransformer action {action} not in ["keep", "drop"]'
+    assert isinstance(column_list, list), f'DropColumnsTransformer expected list but saw {type(column_list)}'
+    self.column_list = column_list
+    self.action = action
+
+  #fill in rest below
+  def transform(self, X):
+    Table = X.copy()
+    #get columbs for keeping
+
+    if self.action == 'drop':
+      Table = Table.drop(columns = self.column_list)
+    else:
+      Table = Table[self.column_list]
+    return Table
+  
+  def fit(self, X, y = None): 
+    print("Warning:DropColumnsTransformer.fit does nothing.")
+    return X
+    
+
+  def fit_transform(self, X, y= None):
+    result = self.transform(X)
+    return result
