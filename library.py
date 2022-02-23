1df727d4ea0e7c1cab7919702a126bc3851bb785
ChrysalisSC <80729716+ChyrsalisSC@users.noreply.github.com>
2022-02-22 18:26:34 -0800

    Weird commit issues? threshold and having search fixed

--- library.py
+++ library.py
@@ -33,34 +33,7 @@ class OHETransformer(BaseEstimator, TransformerMixin):
 
   def transform(self, X):
     assert isinstance(X, pd.core.frame.DataFrame), f'RenamingTransformer.transform expected Dataframe but got {type(X)} instead.'
-    
-def halving_search(model, grid, x_train, y_train, factor=3, scoring='roc_auc'):
-
-    halving_cv = HalvingGridSearchCV(
-      model, grid,  #our model and the parameter combos we want to try
-      scoring = scoring,  #could alternatively choose f1, accuracy or others
-      n_jobs=-1,
-      min_resources="exhaust",
-      factor = factor,  #a typical place to start so triple samples and take top 3rd of combos on each iteration
-      cv=5, random_state=1234,
-      refit=True  #remembers the best combo and gives us back that model already trained and ready for testing
-    )
-    grid_result = halving_cv.fit(x_train ,  y_train)
-    return grid_result
   
-def threshold_results(thresh_list, actuals, predicted):
-  result_df = pd.DataFrame(columns=['threshold', 'precision', 'recall', 'f1', 'accuracy'])
-  for t in thresh_list:
-    yhat = [1 if v >=t else 0 for v in predicted]
-    #note: where TP=0, the Precision and Recall both become 0
-    precision = precision_score(actuals, yhat, zero_division=0)
-    recall = recall_score(actuals, yhat, zero_division=0)
-    f1 = f1_score(actuals, yhat)
-    accuracy = accuracy_score(actuals, yhat)
-    result_df.loc[len(result_df)] = {'threshold':t, 'precision':precision, 'recall':recall, 'f1':f1, 'accuracy':accuracy}
-  return result_df
-  
-
     X_ = X.copy()
     X_ = pd.get_dummies(X_,
                                prefix=self.target_column,   
@@ -320,3 +293,30 @@ def customer_setup(customer_table, transformer=customer_transformer, rs=107, ts=
                                                                            customer_table['Rating'].to_list(),
                                                                            customer_transformer,rs, ts)
     return x_trained_numpy, y_train_numpy, x_test_numpy, y_test_numpy
+  
+def threshold_results(thresh_list, actuals, predicted):
+  result_df = pd.DataFrame(columns=['threshold', 'precision', 'recall', 'f1', 'accuracy'])
+  for t in thresh_list:
+    yhat = [1 if v >=t else 0 for v in predicted]
+    #note: where TP=0, the Precision and Recall both become 0
+    precision = precision_score(actuals, yhat, zero_division=0)
+    recall = recall_score(actuals, yhat, zero_division=0)
+    f1 = f1_score(actuals, yhat)
+    accuracy = accuracy_score(actuals, yhat)
+    result_df.loc[len(result_df)] = {'threshold':t, 'precision':precision, 'recall':recall, 'f1':f1, 'accuracy':accuracy}
+  return result_df
+
+
+def halving_search(model, grid, x_train, y_train, factor=3, scoring='roc_auc'):
+
+    halving_cv = HalvingGridSearchCV(
+      model, grid,  #our model and the parameter combos we want to try
+      scoring = scoring,  #could alternatively choose f1, accuracy or others
+      n_jobs=-1,
+      min_resources="exhaust",
+      factor = factor,  #a typical place to start so triple samples and take top 3rd of combos on each iteration
+      cv=5, random_state=1234,
+      refit=True  #remembers the best combo and gives us back that model already trained and ready for testing
+    )
+    grid_result = halving_cv.fit(x_train ,  y_train)
+    return grid_result
